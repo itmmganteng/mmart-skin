15a9ae054dfa0bbae2356fc1db4364384219a4ff
Dwi Supartama <dwisupartama3@gmail.com>
2024-02-07 08:51:45 +0800

    update menu

--- src/skin-stubs/resource/views/layout/partials/sidebar-layout/_sidebar.blade.php
+++ src/skin-stubs/resource/views/layout/partials/sidebar-layout/_sidebar.blade.php
@@ -1,7 +1,11 @@
 <!--begin::Sidebar-->
 <div id="kt_app_sidebar" class="app-sidebar flex-column" data-kt-drawer="true" data-kt-drawer-name="app-sidebar" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="225px" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_mobile_toggle">
 	@include(config('settings.KT_THEME_LAYOUT_DIR').'/partials/sidebar-layout/sidebar/_logo')
-	@include(config('settings.KT_THEME_LAYOUT_DIR').'/partials/sidebar-layout/sidebar/_menu')
+	@if(config('menu.KT_MENU_MODE') == 'auto')
+		@include(config('settings.KT_THEME_LAYOUT_DIR').'/partials/sidebar-layout/sidebar/_menu')
+	@elseif(config('menu.KT_MENU_MODE') == 'manual')
+		@include(config('settings.KT_THEME_LAYOUT_DIR').'/partials/sidebar-layout/sidebar/_menu-manual')
+	@endif
 	@include(config('settings.KT_THEME_LAYOUT_DIR').'/partials/sidebar-layout/sidebar/_footer')
 </div>
 <!--end::Sidebar-->
--- src/skin-stubs/resource/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ src/skin-stubs/resource/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -4,7 +4,7 @@
     <div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
         <!--begin::Menu-->
         <div class="menu menu-column menu-rounded menu-sub-indention px-3 fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
-            {!! renderMenus() !!}
+            {!! generateMenu() !!}
         </div>
         <!--end::Menu-->
     </div>
